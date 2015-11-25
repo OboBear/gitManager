d9c625533fff45eadc44939addbc5ed53f3abb2c
uuu1993 <634468460@qq.com>
2015-11-25 23:53:56 +0800

    add update

--- code.py
+++ code.py
@@ -5,7 +5,8 @@ import commands
 urls=(
     '/','index',
     '/update','update',
-    '/command','command',    
+    '/command','command',
+    '/show','show'
 )
 
 
@@ -26,10 +27,18 @@ class command:
         print 'command:'+commandName+'\n'
         if commandName=="help":
             return ''' show git:列出仓库列表\n show auth:列出授权公钥\n create git'''
-        return 'command'    
+        return 'command'
+class show:
+    def POST(self):
+        postParams = web.input();
+        commandName = postParams.get('request')
+        if commandName == 'repo':
+            (status, output) = commands.getstatusoutput('ls ~/gitRepository/pythonServer/')
+            return output;
+        if commandName == 'auth'
+            (status, output) = commands.getstatusoutput('cat ~/.ssh/authorized_keys')
+            return output;
         
-
-
 
 if __name__ == "__main__":
     app = web.application(urls,globals())
