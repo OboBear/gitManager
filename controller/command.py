72ae25ee10c591d971aaa36472f30f083186ca2d
uuu1993 <634468460@qq.com>
2015-12-03 23:48:37 +0800

    自动上传++fix_bug

--- controller/command.py
+++ controller/command.py
@@ -10,4 +10,10 @@ class command:
         print 'command:'+commandName+'\n'
         if commandName=="help":
             return ''' repo:列出git仓库列表\n auth:列出授权公钥\n'''
+        if commandName == 'repo':
+            (status, output) = commands.getstatusoutput('ls ~/gitRepository/pythonServer/')
+            return output;
+        if commandName == 'auth':
+            (status, output) = commands.getstatusoutput('cat ~/.ssh/authorized_keys')
+            return output;
         return 'command'
