2df964a7590ef77a0477cb6e9606465d2aa48177
uuu1993 <634468460@qq.com>
2015-12-03 22:23:11 +0800

    自动上传++separatetheclass

--- code.py
+++ code.py
@@ -3,6 +3,8 @@
 import web
 import commands
 import controller/index
+
+
 urls=(
     '/','index',
     '/command','command',
@@ -12,25 +14,9 @@ urls=(
 
 
 
-class command:
-    def POST(self):
-        postParams = web.input();
-        commandName = postParams.get('command')
-        print 'command:'+commandName+'\n'
-        if commandName=="help":
-            return ''' show git:列出仓库列表\n show auth:列出授权公钥\n create git'''
-        return 'command'
 
-class show:
-    def POST(self):
-        postParams = web.input();
-        commandName = postParams.get('request')
-        if commandName == 'repo':
-            (status, output) = commands.getstatusoutput('ls ~/gitRepository/pythonServer/')
-            return output;
-        if commandName == 'auth'
-            (status, output) = commands.getstatusoutput('cat ~/.ssh/authorized_keys')
-            return output;
+
+
         
 
 if __name__ == "__main__":
