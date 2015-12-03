e128dbfad888ad0aff721b00905532085e1d5cc4
uuu1993 <634468460@qq.com>
2015-12-03 22:36:01 +0800

    自动上传++create

--- controller/index.py
+++ controller/index.py
@@ -3,6 +3,6 @@ import commands
 
 class index:
     def GET(self):
-    render = web.template.render('templates/')
-    name = 'index'
+    render=web.template.render('templates/')
+    name='index'
     return render.index()
